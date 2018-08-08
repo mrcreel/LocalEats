3ffd9db4b52ed69fa49b5885aab3b96c6de45d62
mrcreel <michaelrcreel@gmail.com>
2018-08-08 15:36:21 -0500

    Clear old cache

--- dist/sw.js
+++ dist/sw.js
@@ -11,22 +11,37 @@ var urlsToCache =
     './js/map.js',
     './js/header.js',
     './js/restaurant_info.js',
-
   ];
 
 // Install Service Worker
 self.addEventListener("install", function(e) {
   console.log('Service worker: Installed')
   e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches
+    .open(CACHE_NAME)
+    .then(function(cache) {
+      console.log('Service Worker: Caching Files');
+      cache.addAll(urlsToCache);
+    })
+    .then(() => self.skipWaiting())
   );
 });
 
 // Activate Service Worker
 self.addEventListener("activate", function(e) {
   console.log('Service worker: Activated')
+  //Remove olde chache
+  e.waitUntil(
+    caches.keys()
+    .then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if(cache !== CACHE_NAME){
+            console.log(`Service Worker: Cache ${cache} Cleared`)
+            return caches.delete(cache)
+          }
+        })
+      )
+    })
+  )
 })
