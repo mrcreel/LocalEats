9068ccbd8b45e5255256fe9f8f2923195da9ce2f
mrcreel <michaelrcreel@gmail.com>
2018-08-10 10:51:49 -0500

    Update sw.js

--- dist/sw.js
+++ dist/sw.js
@@ -1,42 +1,4 @@
-<<<<<<< HEAD
-const cacheName = "v1";
 
-const CACHE_URLS = [
-  "index.html",
-  "restaurant.html",
-  "./css/main.css",
-  "./js/main.js",
-  "./js/header.js",
-  "./js/dbhelper.js",
-  "./js/map.js",
-  "./js/restaurant_info.js",
-  './data/restaurants.jsom'
-];
-
-// Install SW
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches
-      .open(cacheName)
-      .then(cache => {
-        console.log(`SW Caching files`);
-        cache.addAll(CACHE_URLS);
-      })
-      .then(() => self.skipWaiting())
-  );
-});
-
-// Activate SW
-self.addEventListener("activate", event => {
-  console.log("SW Activated");
-  //Delete old caches
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache != cacheName) {
-            console.log(`SW: Clear old cache`);
-=======
 var CACHE_NAME = "v1";
 
 // Install Service Worker
@@ -54,23 +16,11 @@ self.addEventListener("activate", function(e) {
         cacheNames.map(cache => {
           if (cache !== CACHE_NAME) {
             console.log(`Service Worker: Cache ${cache} Cleared`);
->>>>>>> fetchAll
             return caches.delete(cache);
           }
         })
       );
     })
-<<<<<<< HEAD
-  );
-});
-
-// SW Fetch
-self.addEventListener("fetch", event => {
-  console.log(`SW: Fetching`);
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-=======
->>>>>>> fetchAll
   );
 });
 
