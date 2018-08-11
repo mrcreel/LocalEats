9be5e42a94b03fd79a788fb860eb3d2719c99d89
mrcreel <michaelrcreel@gmail.com>
2018-08-11 15:12:26 -0500

    Minify SASS to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,32 @@
 const gulp = require('gulp');
 
+const uglify = require('gulp-uglify');
+const csso = require('gulp-csso');
+
+const rename = require('gulp-rename');
+
+const sass = require('gulp-sass');
+
 const webp = require('gulp-webp');
 const image = require('gulp-image');
-
-gulp.task('test', function() {
-  console.log('Gulp js is running');
-});
 
 gulp.task('images', function () {
   gulp.src('./img/*.jpg')
   .pipe(image())
   .pipe(webp())
   .pipe(gulp.dest('dist/img'))
+});
+
+
+gulp.task('sass', function() {
+  gulp.src('./scss/**/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./dist/css'))
+  .pipe(rename('main.min.css'))
+  .pipe(csso())
+  .pipe(gulp.dest('./dist/css'))
+});
+
+gulp.task('watch', ['sass'], function(){
+  gulp.watch('./scss/**/*.scss', ['sass'])
 });
