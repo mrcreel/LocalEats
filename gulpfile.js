5edc1b0443c07f4dc63d4970e2908fc167515d0f
mrcreel <michaelrcreel@gmail.com>
2018-08-11 16:32:18 -0500

    concat js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp');
 
+var pump = require('pump');
+
+const concat = require('gulp-concat');
+
 const uglify = require('gulp-uglify');
 const csso = require('gulp-csso');
 
@@ -21,12 +25,24 @@ gulp.task('images', function () {
 gulp.task('sass', function() {
   gulp.src('./scss/**/*.scss')
   .pipe(sass())
-  .pipe(gulp.dest('./dist/css'))
-  .pipe(rename('main.min.css'))
   .pipe(csso())
+  .pipe(rename('main.min.css'))
   .pipe(gulp.dest('./dist/css'))
 });
 
-gulp.task('watch', ['sass'], function(){
-  gulp.watch('./scss/**/*.scss', ['sass'])
+gulp.task('scripts-main', function(){
+  gulp.src(['./js/dbhelper.js', './js/main.js', './js/header.js'])
+  .pipe(concat('main.min.js'))
+  .pipe(gulp.dest('./dist/js'))
+})
+
+gulp.task('mini', function(){
+  gulp.src('./dist/js/main/min.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./dist/js'))
+})
+
+gulp.task('watch', ['sass', 'scripts-main',], function(){
+  gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch('./js/*.js', ['scripts-main']);
 });
